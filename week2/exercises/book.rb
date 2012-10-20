dfbde4a9178d7ec36d3daece1482ec240eacc786
Ryan Calderon <rcalderon04@gmail.com>
2012-10-20 13:19:19 -0700

    Changes from in class exercise

--- week2/exercises/book.rb
+++ week2/exercises/book.rb
@@ -1,2 +1,19 @@
 class Book
+  attr_accessor :title, :pageCount
+  # make the argument not required
+  #def initialize(title="Not Set")
+  def initialize(title, pageCount)
+    @title = title
+    @pages = pageCount
+  end
+  def page_count
+    "Page count is #{@pages}" 
+  end
+  #readers
+  #def title
+   # @title
+  #end
+  #writers
+  #def title=(title)
+  #  @title = title
 end
